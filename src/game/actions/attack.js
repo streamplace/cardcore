74163d5b77bf71275b567fdf4837976d18b77933
Eli Mallon <eli@stream.place>
2018-07-09 18:32:35 -0700

    fix attacking

--- src/game/actions/attack.js
+++ src/game/actions/attack.js
@@ -1,16 +1,29 @@
-import { checkDeath } from "./check-death";
+import { CHECK_DEATH } from "./check-death";
+import { START_GAME } from "./start-game";
 
 export const ATTACK = "ATTACK";
-export const attack = (attackingUnitId, defendingUnitId) => async dispatch => {
-  await dispatch({
+export const attack = (attackingUnitId, defendingUnitId) => {
+  return {
     type: ATTACK,
     attackingUnitId,
     defendingUnitId
-  });
-  dispatch(checkDeath());
+  };
 };
 
 export const attackReducer = (state, action) => {
+  if (action.type === START_GAME) {
+    return {
+      ...state,
+      game: {
+        ...state.game,
+        allowedActions: {
+          ...state.game.allowedActions,
+          [ATTACK]: true
+        }
+      }
+    };
+  }
+
   if (action.type === ATTACK) {
     const { attackingUnitId, defendingUnitId } = action;
     const attackingUnit = state.game.units[attackingUnitId];
@@ -30,7 +43,15 @@ export const attackReducer = (state, action) => {
             ...defendingUnit,
             health: defendingUnit.health - attackingUnit.attack
           }
-        }
+        },
+        nextActions: [
+          {
+            playerId: action._sender,
+            action: {
+              type: CHECK_DEATH
+            }
+          }
+        ]
       }
     };
   }
