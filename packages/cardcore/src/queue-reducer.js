2bfe84b63d6352d5a91c2ba8a0e0afc41e6798e5
Eli Mallon <eli@stream.place>
2018-08-19 18:10:41 -0700

    try a schema validator that doesn't use code generation

--- packages/cardcore/src/queue-reducer.js
+++ packages/cardcore/src/queue-reducer.js
@@ -1,14 +1,17 @@
-import Ajv from "ajv";
-const ajv = new Ajv({ allErrors: true });
+// import Ajv from "ajv";
+// const ajv = new Ajv({ allErrors: true });
+import ZSchema from "z-schema";
+const validator = new ZSchema();
 
 export default function queueReducer(state, action) {
   if (state.game.queue && state.game.queue.length > 0) {
     const schema = state.game.queue[0];
-    const validate = ajv.compile(schema);
-    const valid = validate(action);
+    // const validate = ajv.compile(schema);
+
+    const valid = validator.validate(action, schema);
     if (!valid) {
       throw new Error(
-        JSON.stringify({ action, errors: validate.errors }, null, 2)
+        JSON.stringify({ action, errors: validator.getLastErrors() }, null, 2)
       );
     }
   }
