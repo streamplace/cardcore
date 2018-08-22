d3710aabab15e7d30b2ab378e87c726c4adc6fff
Eli Mallon <eli@stream.place>
2018-08-21 21:20:09 -0700

    working native yarn workspace

--- packages/native/crna-entry.js
+++ packages/native/crna-entry.js
@@ -1,6 +1,7 @@
-import App from "./src/native.js";
+import "./polyfills";
 import Expo from "expo";
 import React from "react";
+import App from "./dist/native.js";
 
 const AwakeInDevApp = props => [
   <App key="app" {...props} />,
