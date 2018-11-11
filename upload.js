400e87433bc54a8029a8bb194e9a2527b11963df
Eli Mallon <eli@stream.place>
2018-11-11 00:35:49 -0800

    slack notify errors

--- upload.js
+++ upload.js
@@ -1,8 +1,20 @@
 const azure = require("azure-storage");
+const fetch = require("isomorphic-fetch");
 const blobService = azure.createBlobService(process.env.AZ_CONNECTION_STRING);
 
 const container = "cardcore-builds";
 const filename = "cardcore-error.json";
+
+const slackNotify = async function(text) {
+  if (typeof text !== "string") {
+    text = JSON.stringify(text);
+  }
+  await fetch(slack.rootNotifications, {
+    method: "POST",
+    body: JSON.stringify({ text }),
+    headers: { "content-type": "application/json" }
+  });
+};
 
 blobService.createContainerIfNotExists(container, error => {
   if (error) return console.log(error);
@@ -12,6 +24,7 @@ blobService.createContainerIfNotExists(container, error => {
     `${process.env.DRONE_COMMIT}-error.json`,
     (error, result) => {
       if (error) return console.log(error);
+      console.log(slackNotify("test"));
       console.dir(result, { depth: null, colors: true });
     }
   );
